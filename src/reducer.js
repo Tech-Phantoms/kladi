00897cab6683bcb248fd68857c4a18ec672f1ec6
Abir <abir.pal899@gmail.com>
2020-08-31 02:27:34 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,22 @@
-import { CREATE, UPDATE } from './constants';
+import { CREATE, UPDATE } from './constants'
 
 export default (state, action) => {
   switch (action.type) {
-    case CREATE:
-      let newState = { ...state };
-      newState[action.name] = action.value;
-      return newState;
-    case UPDATE:
+    case CREATE: {
+      let newState = { ...state }
+      newState[action.name] = action.value
+      return newState
+    }
+    case UPDATE: {
       let ns = { ...state }
-      ns[action.name] = action.value;
-      return ns;
-    default:
-      return state;
+      ns[action.name] = action.value
+      return ns
+    }
+    default: {
+      return state
+    }
   }
 }
+
+
+
