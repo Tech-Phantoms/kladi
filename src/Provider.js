c35455c36efb0498ff52224711129bf8f2d65e6d
Abir <abir.pal899@gmail.com>
2020-08-31 02:23:49 +0530

    Update Provider.js

--- src/Provider.js
+++ src/Provider.js
@@ -2,8 +2,8 @@ import store from './context'
 import React, { useReducer } from 'react'
 import reducer from './reducer'
 
-const Provider = ({ children, initialState = {} }) => { const [state, dispatch] = useReducer(reducer, initialState);
-return <store.Provider value={{ state, dispatch }}>{children}</store.Provider>
+const Provider = ({ children, initialState = {} }) => { const [state, dispatch] = useReducer(reducer, initialState)
+  return <store.Provider value={{ state, dispatch }}>{children}</store.Provider>
 }
-export default Provider;
+export default Provider
 
