fb09d13f6dd5b6e0fddcd4c51a128d7502a025ee
Abir <abir.pal899@gmail.com>
2020-08-31 02:00:14 +0530

    TravisCI Linting Fix-HooksJS

--- src/hooks.js
+++ src/hooks.js
@@ -1,27 +1,19 @@
-import store from './context'
-import { useContext } from 'react'
-import { CREATE, UPDATE } from './constants'
+import store from './context';
+import { useContext } from 'react';
+import { CREATE, UPDATE } from './constants';
 
 const useState = (name, value) => {
-
-    const { state, dispatch } = useContext(store)
-
-
-    const changer = name => newVal => {
-        dispatch({ type: UPDATE, name: name, value: newVal })
-        console.log('useState', state)
+  const { state, dispatch } = useContext(store);
+  const changer = name => newVal => {
+        dispatch({ type: UPDATE, name: name, value: newVal });
+        console.log('useState', state);
     }
-
-    if (state[name] === undefined) {
-        dispatch({ type: CREATE, name: name, value: value })
-        console.log('useState', state)
-
-        return [state[name], changer(name)]
+  if (state[name] === undefined) {
+     dispatch({ type: CREATE, name: name, value: value });
+     console.log('useState', state);
+    return [state[name], changer(name)];
     } else {
-        return [state[name], changer(name)]
+      return [state[name], changer(name)];
     }
-
 }
-
-
-export { useState }
+export { useState };
