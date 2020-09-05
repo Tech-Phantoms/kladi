4a9c695d357cb167edd6529a7ade204597a93308
Souvik <souvikde.ns@gmail.com>
2020-09-05 05:58:31 +0530

    removed log statements 🎉

--- src/hooks.js
+++ src/hooks.js
@@ -6,11 +6,9 @@ const useState = (name, value) => {
   const { state, dispatch } = useContext(store)
   const changer = (name) => (newVal) => {
     dispatch({ type: UPDATE, name: name, value: newVal })
-    console.log('useState', state)
   }
   if (state[name] === undefined) {
     dispatch({ type: CREATE, name: name, value: value })
-    console.log('useState', state)
     return [state[name], changer(name)]
   } else {
     return [state[name], changer(name)]
