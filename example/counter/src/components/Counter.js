73cb58d211f5a425d6c8d0b743603c7eb99dc7c2
Lakshya Khera <lakshya.khera@gmail.com>
2020-09-15 22:55:52 +0530

    feat: Added a static counter example

---
+++ example/counter/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../App.css'
+export default class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>1</h1>
+        <button>-</button>
+        <button>+</button>
+      </div>
+    )
+  }
+}
---
+++ example/counter/src/components/JoinUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class JoinUs extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <h4>For more information check out our documentation!</h4>
+        <h4>Make sure to check out Github repository as well!</h4>
+        
+      </div>
+    )
+  }
+}
