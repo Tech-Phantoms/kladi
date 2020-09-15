3483196ab1742356724b3fc62d38d61990fb3e4f
Abir <abir.pal899@gmail.com>
2020-09-16 00:04:02 +0530

    Update JoinUs.js

--- example/counter/src/components/JoinUs.js
+++ example/counter/src/components/JoinUs.js
@@ -4,7 +4,7 @@ export default class JoinUs extends Component {
   render() {
     return (
       <div className="footer">
-        <h4>For more information check out our documentation!</h4>
+        <h4>An Open Source Project by Tech Phantoms</h4>
         <h4>Make sure to check out Github repository as well!</h4>
         
       </div>
